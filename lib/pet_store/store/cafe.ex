b10cc586f94d0b2d3ea9fd5f5039154577d482f7
Ryan Cua <ry.j.cua@gmail.com>
2025-12-04 18:50:35 -0800

    update jobs and add additional field for one of the db

--- lib/pet_store/store/cafe.ex
+++ lib/pet_store/store/cafe.ex
@@ -1,12 +1,16 @@
 defmodule PetStore.Store.Cafe do
   use Ecto.Schema
   import Ecto.Changeset
-
+  alias PetStore.Store.Cafe
   schema "store_cafe" do
     field :item, :string
+    field :item_name, :string
+
     field :menu_type, :string
     field :add_ons, :string
     field :price, :decimal
+
+
 
     timestamps(type: :utc_datetime)
   end
@@ -14,7 +18,17 @@ defmodule PetStore.Store.Cafe do
   @doc false
   def changeset(cafe, attrs) do
     cafe
-    |> cast(attrs, [:item, :menu_type, :add_ons, :price])
-    |> validate_required([:item, :menu_type, :add_ons, :price])
+    |> cast(attrs, [:item, :menu_type, :add_ons, :price, :item_name])
+    |> validate_required([:item, :menu_type, :add_ons, :price, :item_name])
+    |> unique_constraint(:item_name)
   end
+
+  def get_items_by_type(item_type, limit \\ 0, offset \\ 0) do
+    Cafe |> where([c], c.menu_type == ^item_type)  |> Repo.all(limit: ^limit, offset: ^offset)
+  end
+
+  def get_item_by_name(item_name) do
+    Cafe |> where([c], c.item_name == ^item_name) |> Repo.one()
+  end
+
 end
